1f5241c8c15d9d15e1bdc1ed55497ea7b1b57c29
Jemvaris <jemchilove@yahoo.com>
2022-06-26 08:22:21 +0100

    key commit

--- I4G015765TXN/blog/views.py
+++ I4G015765TXN/blog/views.py
@@ -1,8 +1,6 @@
+
 from django.shortcuts import render
-from blog.models import Post
-from django.views.generic.edit import DeleteView
-from django.views.generic.edit import CreateView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views import generic
@@ -14,10 +12,6 @@ class PostListView(ListView):
 
     # specify the model for list view
     model = Post
-
-    # specify the fields to be displayed
-    fields = ['__all__']
-    template = 'blog/post_list.html'
     
 # Create another view, PostCreateView, which inherits django’s generic CreateView, with attributes:
 
@@ -27,35 +21,31 @@ class PostCreateView(CreateView):
     model = Post
 
     # specify the fields to be displayed
-    fields = ['__all__']
+    fields = '__all__'
 
-    success_url  = 'reverse_lazy(“blog:all”)'
-    template = 'blog/post_form.html'
+    success_url  = reverse_lazy('blog:all')
+    
 
 
 # Create another view, PostDetailView, which inherits django’s generic DetailView, with attributes:
 
-class PosDetailView(DetailView):
+class PostDetailView(DetailView):
     model = Post
-
-    fields = ['__all__']
-    template_name = 'blog/post_detail.html'
     
 
 #Create another view PostUpdateView, which inherits django’s generic UpdateView, with attributes:
 
-class PosUpdateView(UpdateView):
+class PostUpdateView(UpdateView):
     model = Post
 
-    fields = ['__all__']
-    template_name = 'blog/post_form.html'
-    success_url  = 'reverse_lazy(“blog:all”)'
+    fields = '__all__'
+    
+    success_url  = reverse_lazy('blog:all')
 
 #Create another view PostDeleteView, which inherits django’s generic UpdateView, with attributes:
 
-class PosDeleteView(DeleteView):
+class PostDeleteView(DeleteView):
     model = Post
 
-    fields = ['__all__']
-    template_name = 'post_confirm_delete.html'
-    success_url  = 'reverse_lazy(“blog:all”)'
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
